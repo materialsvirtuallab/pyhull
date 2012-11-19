3d6147a599ca710fd6b05923099a016d51fd86ff
Shyue Ping Ong <shyue@mit.edu>
2012-11-19 14:19:46 -0500

    Add a parallel benchmarking.

---
+++ benchmarking/parallel_benchmark.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""
+TODO: Change the module doc.
+"""
+
+from __future__ import division
+
+__author__ = "shyuepingong"
+__version__ = "0.1"
+__maintainer__ = "Shyue Ping Ong"
+__email__ = "shyuep@gmail.com"
+__status__ = "Beta"
+__date__ = "11/19/12"
+
+from multiprocessing import Pool
+
+import numpy as np
+
+from pyhull.qconvex import ConvexHull
+
+num_proc = 100
+
+def get_vertices(data):
+    return ConvexHull(data).vertices
+
+def serial_test(data):
+    d = []
+    for i in xrange(num_proc):
+        d.append(get_vertices(data))
+    print "Serial {}".format(d[0][0])
+    return d
+
+def parallel_test(data):
+    p = Pool(4)
+    all_data = p.map(get_vertices, [data] * num_proc)
+    for d in all_data:
+        print d[0]
+
+if __name__ == "__main__":
+    import timeit
+    global data
+    for npts in [100, 1000]:
+        for dim in [3,4]:
+            data = np.random.randn(npts,dim)
+            t = timeit.timeit("serial_test(data)",
+                              setup="from __main__ import serial_test, data",
+                              number=1)
+            print ("Serial: {} {} {:.5f}".format(npts, dim, t))
+            t = timeit.timeit("parallel_test(data)",
+                              setup="from __main__ import parallel_test, data",
+                              number=1)
+            print ("Parallel: {} {} {:.5f}".format(npts, dim, t))
