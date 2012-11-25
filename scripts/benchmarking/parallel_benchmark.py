513566107b52455719a876ec02f1255145f06a78
Shyue Ping Ong <shyuep@gmail.com>
2012-11-24 21:03:20 -0500

    Improve benchmarking.

--- scripts/benchmarking/parallel_benchmark.py
+++ scripts/benchmarking/parallel_benchmark.py
@@ -33,18 +33,18 @@ def serial_test(data):
 def parallel_test(data):
     p = Pool(4)
     all_data = p.map(get_vertices, [data] * num_proc)
-    print all([all_data[i] == all_data[0] for i in xrange(num_proc)])
+    assert all([all_data[i] == all_data[0] for i in xrange(num_proc)])
 
 if __name__ == "__main__":
     import timeit
     global data
-    for npts in [100, 1000]:
-        for dim in [3, 4]:
+    for npts in [100, 1000, 10000]:
+        for dim in [3, 4, 5]:
             data = np.random.randn(npts,dim)
             t = timeit.timeit("serial_test(data)",
                               setup="from __main__ import serial_test, data",
                               number=1)
-            print ("Serial: {} {} {:.5f}".format(npts, dim, t))
+            print ("Serial:   {} {} {:.5f}".format(npts, dim, t))
             t = timeit.timeit("parallel_test(data)",
                               setup="from __main__ import parallel_test, data",
                               number=1)
