Bug fix for precision in converting floats to string.

pyhull/halfspace.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 """
 This module defines classes for computing halfspace intersections
 """
@@ -22,11 +20,11 @@ class Halfspace(object):
     """
     def __init__(self, normal, offset):
         """
+        Initializes a Halfspace.
+
         Args:
-            normal:
-                vector normal to hyperplane
-            offset:
-                offset of hyperplane from origin
+            normal: vector normal to hyperplane
+            offset: offset of hyperplane from origin
         """
         self.normal = normal
         self.offset = offset
@@ -41,14 +39,10 @@ class Halfspace(object):
         bounding hyperplane.
 
         Args:
-            basis:
-                basis for the hyperplane (array with vector rows)
-            origin:
-                point on the hyperplane
-            point:
-                point not on the hyperplane
-            internal:
-                whether point is inside the halfspace
+            basis: basis for the hyperplane (array with vector rows)
+            origin: point on the hyperplane
+            point: point not on the hyperplane
+            internal: whether point is inside the halfspace
         """
         basis = np.array(basis)
         assert basis.shape[0] + 1 == basis.shape[1]

pyhull/simplex.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 """
 This module defines a class representing an arbitrary Simplex in arbitrary
 dimensional space.
@@ -25,10 +23,11 @@ class Simplex(object):
 
     def __init__(self, coords):
         """
+        Initializes a Simplex from coordinates.
+
         Args:
-            coords:
-                Coords of the vertices of the simplex. E.g.,
-                [[1,2,3],[2,4,5],[6,7,8]].
+            coords ([[float]]): Coords of the vertices of the simplex. E.g.,
+                [[1, 2, 3], [2, 4, 5], [6, 7, 8]].
         """
         self.simplex_dim = len(coords) - 1
         self.space_dim = len(coords[0])
@@ -60,10 +59,8 @@ class Simplex(object):
         1e-8 by default) for numerical errors.
 
         Args:
-            point:
-                Point to test
-            tolerance:
-                Tolerance to test if point is in simplex.
+            point ([float]): Point to test
+            tolerance (float): Tolerance to test if point is in simplex.
         """
         origin = self._coords[0]
         bary_coords = np.array([self._coords[i] - origin