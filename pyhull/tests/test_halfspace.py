More fixes for precision and bad code naming.

pyhull/tests/test_halfspace.py
@@ -37,7 +37,7 @@ class HalfspaceTest(unittest.TestCase):
 
         for h, vs in zip(hi.halfspaces, hi.facets_by_halfspace):
             for v in vs:
-                self.assertAlmostEqual(np.dot(h.normal, hi.vertices[v]) + h.offset, 0) 
+                self.assertAlmostEqual(np.dot(h.normal, hi.vertices[v]) + h.offset, 0)
         for v, hss in zip(hi.vertices, hi.facets_by_vertex):
             for i in hss:
                 hs = hi.halfspaces[i]
@@ -52,7 +52,7 @@ class HalfspaceTest(unittest.TestCase):
 
         for h, vs in zip(hi.halfspaces, hi.facets_by_halfspace):
             for v in vs:
-                self.assertAlmostEqual(np.dot(h.normal, hi.vertices[v]) + h.offset, 0) 
+                self.assertAlmostEqual(np.dot(h.normal, hi.vertices[v]) + h.offset, 0)
         for v, hss in zip(hi.vertices, hi.facets_by_vertex):
             for i in hss:
                 hs = hi.halfspaces[i]
@@ -69,10 +69,10 @@ class HalfspaceTest(unittest.TestCase):
         h2 = Halfspace.from_hyperplane([[0,1,0], [0,0,1]], [1,1,1], [0.9, 0.9, 0.9], True)
         h3 = Halfspace.from_hyperplane([[0,0,1], [1,0,0]], [1,1,1], [0.9, 0.9, 0.9], True)
         h4 = Halfspace.from_hyperplane([[1,0,0], [0,1,0]], [2,2,2], [0.9, 0.9, 0.9], True)
-        
+
         hi = HalfspaceIntersection([h1, h2, h3, h4], [0.9, 0.9, 0.9])
         self.assertTrue(np.allclose(np.sum(hi.vertices, axis = 0), [np.inf,np.inf,np.inf]))
-        
+
     def test_non_unit_normals(self):
         h1 = Halfspace([3,0], -3)
         h2 = Halfspace([0,2], 2)
@@ -80,3 +80,7 @@ class HalfspaceTest(unittest.TestCase):
         hi = HalfspaceIntersection([h1, h2, h3], [0.9,-1.1])
         self.assertTrue(np.any(np.all(hi.vertices == np.array([1,-2]), axis=1)))
         self.assertTrue(np.any(np.all(hi.vertices == np.array([1,-1]), axis=1)))
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()